b7b24eaff0e64cbe985a58527b63cb105e41c433
seonhoLee-ms <seonho_lee@korea.ac.kr>
2020-02-28 18:49:01 +0900

    minor

--- modeling/EnableUserUpgradePlugin.java
+++ modeling/EnableUserUpgradePlugin.java
@@ -3,29 +3,23 @@ public class C {
             "on jbid_io_attr.ATTRIBUTE_ID =jbid_io_attr_text_values.TEXT_ATTR_VALUE_ID where NAME='enabled' and ATTR_VALUE='true'";
     private String SELECT = "select ATTRIBUTE_ID from jbid_io_attr  inner join jbid_io_attr_text_values  " +
             "on jbid_io_attr.ATTRIBUTE_ID =jbid_io_attr_text_values.TEXT_ATTR_VALUE_ID where NAME='enabled' and ATTR_VALUE='true'";
-    private final String REMOVE_ENABLE_ATTRIBUTE = "delete from jbid_io_attr where ATTRIBUTE_ID IN ( ";
-    private final String REMOVE_ENABLE_ATTRIBUTE_VALUE = "delete from jbid_io_attr_text_values where TEXT_ATTR_VALUE_ID IN (";
     private final String COLUMN_ID = "ATTRIBUTE_ID";
 
     private PreparedStatement findItemsStatement = null;
     private PreparedStatement countItemsStatement = null;
-    private PreparedStatement removeItemStatement = null;
-    private PreparedStatement removeValueStatement = null;
 
     @Override
-    public void processUpgrade(String oldVersion, String newVersion) {
+    public void processUpgrade() {
 
         ResultSet rs = null;
         ResultSet count = null;
-        Connection connection = new Connection();
+        Connection connection = new Connection('...');
         boolean success = false;
-        int nb = 0;
         
         if (connection != null) {
             countItemsStatement = connection.prepareStatement(SELECT_COUNT_ENABLE_ATTRIBUTE);
             count = countItemsStatement.executeQuery();
             if (count.next() && count.getInt(1) > 0) {
-                nb = count.getInt(1);
 
                 findItemsStatement = connection.prepareStatement(SELECT_ENABLE_ATTRIBUTE);
                 findItemsStatement.setFetchSize(1000);
@@ -37,19 +31,14 @@ public class C {
                     while (rs.next()) {
                         i++;
                         temp.append(rs.getString(COLUMN_ID));
-                        if (i % 1000 == 0) {
-                            removeBatch(connection, temp.toString());
-                            connection.commit();
-                            LOG.info("Clean in progress : {}/{}", i, nb);
-                            temp = new StringBuilder();
+                        if (i==1000) {
+                          //do sth
                         }else{
-                            temp.append(",");
+                          //do sth
                         }
                     }
-                    if(i% 1000 != 0){
-                        removeBatch(connection, temp.substring(0, temp.lastIndexOf(",")));
-                        connection.commit();
-                        LOG.info("Clean in progress : {}/{}", i, nb);
+                    if(i!=1000){
+                      //do sth
                     }
                 }
                 success = true;
